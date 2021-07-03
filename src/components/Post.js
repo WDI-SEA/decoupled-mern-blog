d4c89f0ce6f60fefd3dcec556db08517b8d39af2
Consecratio <jcasique@gmail.com>
2021-07-02 21:48:53 -0700

    added delete button

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import axios from 'axios'
 
 const Post = (props) => {
+    const apiUrl = 'http://localhost:3001/blog/'
+
+    const deletePost = async (id) => {
+        // setup axios call to hit api /blog/:id
+        await axios.delete(`${apiUrl}${id}`)
+    }
+
+    // setup list of tags
     let tags
     if(props.tags) {
         tags = props.tags.map((tag, index) => {
@@ -16,7 +25,8 @@ const Post = (props) => {
             <h3>{ props.author }</h3>
             <ul>{ tags }</ul>
             <p>{ props.content }</p>
-            <p><a href={`/edit/${props._id}`}>Edit</a></p>
+            <p><a href={`/edit/${props._id}`}>Edit</a>  <a href="/" onClick={() => deletePost(props._id)}>Delete</a></p>
+
         </div>
     )
 }
