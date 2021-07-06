6f5509420daad33e9fee525c755b59d8558f4584
nesbita <ariananesbit@gmail.com>
2021-07-06 17:47:58 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import './App.css';
 import Navigation from './Navigation'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Create from './Create'
 import One from './One'
 import Two from './Two'
 import Three from './Three'
+import Home from './Home'
 
 const App = () => {
   const [blog, setBlog] = useState([])
@@ -21,29 +22,28 @@ const App = () => {
       .catch(err => console.log(err))
   }, [])
 
-  const renderedBlog = blog.map((blog, index) => <div key={index}>{blog.title}</div>)
+  // const renderedBlog = blog.map((blog, index) => <div key={index}>{blog.title}</div>)
 
   return (
       <div>
       <Router>
-
+        
        <h1>Welcome to the Blog Place!</h1>
        <h2>Select a blog post from below to read it</h2>
 
        <Navigation />
 
-        <Route exact path='/' component={App} 
-        render={App} /> 
-    
-        <Route path='/Create' component={Create} />
-          
-        <Route path='/One' component={One} />
-     
-        <Route path='/Two' component={Two} />
-     
-        <Route path='/Three' component={Three} /> 
-
-      {renderedBlog}
+        <Switch>
+          <Route exact path='/' component={Home} /> 
+      
+          <Route path='/Create' component={Create} />
+            
+          <Route path='/One' component={One} />
+      
+          <Route path='/Two' component={Two} />
+      
+          <Route path='/Three' component={Three} />  
+        </Switch>
       </Router>
 </div>  
   );
