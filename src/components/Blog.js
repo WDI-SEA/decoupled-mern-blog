0cf234915416c5786ae75cf27d463edc3de2892c
Consecratio <jcasique@gmail.com>
2021-07-06 18:47:34 -0700

    added styling to blog posts, added ability to see tags and post content

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import './Blog.css'
 
 const Blog = (props) => {
     let listItems
     if(props.posts){
         listItems = props.posts.map((each, index) => {
+            // get a list of tags from each article
+            let tags = each.tags.map(tag => {
+                return (
+                    <li className="btn btn-outline-secondary btn-sm">{tag}</li>
+                )
+            })
+
+            // return a card for each article
             return (
                 <Card className="mb-3" key={index}>
                     <Card.Body>
                         <Card.Title>{ each.title }</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">{ each.author }</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">By: { each.author }</Card.Subtitle>
+                        <Card.Text className="truncate">{ each.content }</Card.Text>
                         <Card.Link href={`/post/${each._id}`}>Read More...</Card.Link>
                     </Card.Body>
+                    <Card.Footer>
+                        Tags: {tags}
+                    </Card.Footer>
                 </Card>
-                // <li key={index}>
-                //     <h4>{ each.title }</h4>
-                //     <h5>By: { each.author }</h5>
-                //     <p><a href={`/post/${each._id}`}>Read More...</a></p>
-                // </li>
             )
         })
     }
-
-    console.log(props)
 
     return (
         <div>
