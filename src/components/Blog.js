6f11c982c6263bbfe16924883ec28b325db6bbd7
die-perez <pzandie@gmail.com>
2021-07-06 19:50:05 -0700

    some styling on home page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,17 @@
 import React from 'react'
+import styled, { css } from 'styled-components'
+
+const PostDivs = styled.div`
+    background-color: white;
+    width: 40%;
+    padding: 20px;
+    margin: 10px auto;
+    border-radius: 15px;
+`
+const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
 
 export default function Blog(props) {
     // console.log(props.tags)
@@ -13,17 +26,17 @@ export default function Blog(props) {
     const renderPosts = props.posts.map((post, index) => {
         return (
 
-            <div key={index}>  
+            <PostDivs key={index}>  
                  <h3><a href={`/blog/${post._id}`}>{post.name}</a></h3>
                 <h6>{ oneTag(post) }</h6>
-            </div>
+            </PostDivs>
         )
     })
 
     return (
-        <div>
+        <Container>
             {renderPosts}
-        </div>
+        </Container>
     )
 }
 
